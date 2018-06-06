fff0f7ee9a6e3f1d41da224668ad151cf013c391
Hibikine Kage <goodsmile314159@gmail.com>
2018-06-07 03:23:28 +0900

    init: first commit

---
+++ Assets/Character.fs
@@ -0,0 +1,6 @@
+﻿namespace FSharp
+open UnityEngine
+
+type Character() =
+    inherit MonoBehaviour()
+    member this.Start() = Debug.Log("Hello World");
---
+++ Assets/Node.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharp
+open UnityEngine
+
+type Node() =
+    inherit MonoBehaviour()
+    //let mutable line : LineRenderer
+    member this.Start() = 
+        let line = this.GetComponent<LineRenderer>()
+        line.setPositions()
+        ()
