51cf45da109047c349b70002521101ce34dcc89e
Hibikine Kage <goodsmile314159@gmail.com>
2019-08-17 17:44:24 +0900

    wip

--- Assets/Node.fs
+++ Assets/Node.fs
@@ -6,5 +6,6 @@ type Node() =
     //let mutable line : LineRenderer
     member this.Start() = 
         let line = this.GetComponent<LineRenderer>()
+        this.transform.position <- this.transform.position + new Vector3(1.0f, 0.0f, 0.0f)
         line.setPositions()
         ()
